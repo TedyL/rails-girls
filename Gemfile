c4885d854d57278147b2281836210ed8f1ae72a2
Tedy L <tedy@vinova.sg>
2014-11-29 12:24:20 +0800

    update files

--- Gemfile
+++ Gemfile
@@ -36,7 +36,6 @@ group :production do
   gem 'pg'
   gem 'rails_12factor'
 end
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require js/bootstrap
+//= require js/npm
 //= require_tree .
